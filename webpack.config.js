bcdc60b1fb5bdee1056dcb97d2f5110658108e22
Jon Beasley <jonbeas@gmail.com>
2020-12-17 11:21:37 +0000

    Production Dev server working

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,10 @@
 const path = require('path')
 
 module.exports = {
-    entry: './src/app.js',
+    entry: '/src/app.js',
     output: {
         path: path.join(__dirname, 'public'),
         filename: 'bundle.js',
-        publicPath: '/'
     },
     module: {
         rules: [
