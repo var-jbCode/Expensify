c330db1a1997fc451c7238bc3912882e7dc8a43f
Jon Beasley <jonbeas@gmail.com>
2020-12-21 14:26:09 +0000

    Formatting amount and date

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -48,7 +48,7 @@ export default class ExpenseForm extends React.Component {
             this.setState(() => ({ error: '' }))
             this.props.onSubmit({
                 description: this.state.description,
-                amount: parseFloat(this.state.amount, 10) * 100,
+                amount: parseFloat(this.state.amount, 10),
                 createdAt: this.state.createdAt.valueOf(),
                 note: this.state.note
             })
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom'
 import { connect } from 'react-redux'
+import moment from 'moment'
+import numeral from 'numeral'
 
 export const ExpenseListItem = ({ description, amount, createdAt, id }) => (
     <div>
         <NavLink to={`/edit/${id}`} activeClassName="is-active" exact={true}>
             <h3>{description}</h3>
         </NavLink>
-        <p> Amount: {amount}, Created Date: {createdAt}</p>
+        <p> <b>Amount:</b>
+            {numeral(amount).format('$0,0.00')}
+            <div />
+            <b>Created: </b>
+            {moment(createdAt).format('Do MMMM, YYYY')}
+        </p>
     </div>
 );
 
