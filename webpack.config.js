02d7cf6554be035e19704477e8ad2c5236cf39d1
Jon Beasley <jonbeas@gmail.com>
2021-02-01 16:32:31 +0000

    Added babel-polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-    entry: '/src/app.js',
+    entry: ['babel-polyfill', '/src/app.js'],
     output: {
         path: path.join(__dirname, 'public', 'dist'),
         filename: 'bundle.js',
